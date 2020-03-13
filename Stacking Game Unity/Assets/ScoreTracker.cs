2a23a7a96968ffbed05c91f551beedbc3c99014d
oryancampbell <55443262+oryancampbell@users.noreply.github.com>
2020-03-13 08:42:05 -0400

    Create Code
    Edited UI and created variables for crate.

---
+++ Stacking Game Unity/Assets/ScoreTracker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+public class ScoreTracker : MonoBehaviour { 
+    public ScoreTracker INSTANCE;
+    public int bestScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(INSTANCE == null)
+    {
+        INSTANCE = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    else
+    {
+        Destroy(this.gameObject);
+    }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
